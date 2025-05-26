1aace7f6af758198eef71ef949c5fc56f08d566b
Daria Chaiun <dariachiun@gmail.com>
2025-05-27 00:55:03 +0300

    додала нову папку

---
+++ lab7/check.py
@@ -0,0 +1,5 @@
+import rasterio
+import numpy as np
+
+print("Rasterio version:", rasterio.__version__)
+print("Numpy version:", np.__version__)
---
+++ lab7_1/geocum2.py
@@ -0,0 +1,42 @@
+import rasterio
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+
+tiles = ["LC085", "LC086"]
+
+methods = ["bilinear", "cubic", "lanczos"]
+
+for tile in tiles:
+    print(f"Processing {tile}...")
+
+    for method in methods:
+        try:
+            # open the GeoTIFF file in read mode
+            with rasterio.open(
+                    f"C:/Users/User/Desktop/Homework/2year/2/AD/lab7_1/output2/concatenated_RGB_image_{tile}.tif") as src:
+                original = src.read()  # 3 bands
+
+            # pan-sharpened file
+            with rasterio.open(
+                    f"C:/Users/User/Desktop/Homework/2year/2/AD/lab7_1/output2/pansharpened_{method}_{tile}.tif") as src:
+                sharpened = src.read()
+
+            # for comparison flatten
+            original_flat = original.flatten()                                                                                                                                       # example: original = np.array([[10, 20], [30, 40]]) --> original_flat = original.flatten() == [10, 20, 30, 40]
+            sharpened_flat = sharpened.flatten()
+
+            # arrays have same length
+            if len(original_flat) != len(sharpened_flat):
+                print(f"Dimension mismatch for {tile} ({method}): Original ({len(original_flat)}), Sharpened ({len(sharpened_flat)})")
+                continue
+
+            rmse = np.sqrt(mean_squared_error(original_flat, sharpened_flat))
+            r2 = r2_score(original_flat, sharpened_flat)
+
+            print(f"{tile} Results for {method}:")
+            print(f"RMSE: {rmse}")
+            print(f"R² Score: {r2}")
+            print("-" * 50)
+
+        except FileNotFoundError as e:
+            print(f"Error loading files for {tile} ({method}): {e}")
